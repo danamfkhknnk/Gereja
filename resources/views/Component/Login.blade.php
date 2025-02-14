ba95c1794befb083367383d1880a7f8a1f55a819
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2025-02-14 16:04:54 +0700

    login frontend

--- resources/views/Component/Login.blade.php
+++ resources/views/Component/Login.blade.php
@@ -4,9 +4,82 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>GEREJA</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    
 </head>
-<body>
-    adada
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    <div class="relative w-full h-screen">
+     <div class="absolute inset-0 flex backdrop-blur-md flex-col justify-center text-center items-center  z-20">
+        <form class="md:w-1/3 max-w-sm bg-secondary/75 p-10 rounded-xl shadow-lg" action="{{url('/aksilogin')}}" method="POST">
+            @csrf          
+            <div class="text-center ">
+              <h3 class="mr-1 mb-2 font-bold text-2xl text-dark uppercase">LOGIN ADMIN</h3>
+              @if (Session::has('message'))
+              <div class="flex items-center p-2 mb-2 text-sm text-blue-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" >
+                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <p class="alert" {{Session::get('alert-class', 'alert-info')}}>{{Session::get('message')}}</p>
+              </div>
+              @endif
+  
+              @if ($errors->any())
+              <div class="flex items-center p-2 mb-2 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                @foreach ($errors->all() as $item)
+                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                  <span class="font-medium">{{$item}}</span>
+                </div>
+                @endforeach
+              </div>
+              @endif
+            </div>
+            
+            <input class="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder="Username" name="text" value="{{ old('username')}}" autoFocus />
+            <input class="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Password" name="password" />
+                        
+            <div class="text-center">
+              <button class="mt-2 bg-primary hover:bg-primary/50 px-6 py-2 text-white uppercase rounded text-xs tracking-wider" name="submit" type="submit">
+                Login
+              </button>
+              <a class="mt-2 bg-primary hover:bg-primary/50 px-6 py-2 text-white uppercase rounded text-xs tracking-wider" href="{{url('/')}}">
+               Kembali
+              </a>
+            </div> 
+          </form>
+     </div>
+     <div class="relative w-full h-full z-[-99]" data-carousel="slide" id="testimonialCarousel">
+      <div class="relative h-full overflow-hidden rounded-lg ">
+    
+       <div class="hidden duration-700 ease-in-out h-full" data-carousel-item>
+        <img src="{{ asset('assets/1.jpg')}}" class="absolute right-0 top-0 w-full object-cover h-full " alt="">
+        
+       </div>
+       <div class="hidden duration-700 ease-in-out h-full" data-carousel-item>
+        <img src="{{ asset('assets/2.jpg')}}" class="absolute right-0 top-0 w-full object-cover h-full " alt="">
+        
+       </div>
+      
+      </div>
+    </div>
+    </div>
+  
+  
+  
+  
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 </body>
 </html>
