db762d00d9f76bd60ff86df0a027bc69667a21d8
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2025-02-14 19:39:37 +0700

    dashboard admin

--- resources/views/Home/Galeri.blade.php
+++ resources/views/Home/Galeri.blade.php
@@ -1,6 +1,6 @@
-<body class="bg-gray-100 flex items-center justify-center min-h-screen">
-    <div class="relative w-full h-screen">
-     <div class="absolute inset-0 flex flex-col justify-center text-center items-center text-white z-20">
+<body class="bg-gray-100 flex items-center justify-center min-h-screen ">
+    <div class="relative w-full h-screen ">
+     <div class="absolute inset-0 flex flex-col justify-center text-center items-center backdrop-blur-sm text-white z-20">
       <p class="lg:text-5xl text-md md:text-3xl font-bold uppercase" data-aos="fade-up">
         GITJ PUNCEL
       </p>
@@ -8,10 +8,10 @@
         Gereja Injili di Tanah Jawa
       </p>
      </div>
-     <div class="relative w-full h-full z-[-99]" data-carousel="slide" id="testimonialCarousel">
+     <div class="relative w-full h-full z-[-99] " data-carousel="slide" id="testimonialCarousel">
       <div class="relative h-full overflow-hidden rounded-lg ">
     
-       <div class="hidden duration-700 ease-in-out h-full" data-carousel-item>
+       <div class="hidden duration-700 ease-in-out h-full " data-carousel-item>
         <img src="{{ asset('assets/1.jpg')}}" class="absolute right-0 top-0 w-full object-cover h-full " alt="">
         
        </div>
